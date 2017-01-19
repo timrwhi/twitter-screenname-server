2aaf08934c16d05db85811c78cfe8189e87b883f
Tim <timrwhi@Tims-MacBook-Air.local>
2017-01-18 22:30:57 -0600

    Adding webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: [__dirname, 'src/app.js'].join('/'),
+  output: { path: [__dirname, 'dist'].join('/'), filename: 'app.js' },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};

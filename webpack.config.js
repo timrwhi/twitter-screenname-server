69f1577d90fcfb3b8bcbea0d87a5fae2c5d82d57
Tim <timrwhi@Tims-MacBook-Air.local>
2017-01-19 22:10:41 -0600

    restore prod build plugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,13 @@ module.exports = {
   devtool: 'cheap-module-source-map',
   entry: path.join(__dirname, '/src/app.js'),
   output: { path: path.join(__dirname, '/dist'), filename: 'app.js' },
-  // plugins: [
-  //   new webpack.DefinePlugin({
-  //     'process.env': {
-  //       'NODE_ENV': JSON.stringify('production')
-  //     }
-  //   })
-  // ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     loaders: [
       {
